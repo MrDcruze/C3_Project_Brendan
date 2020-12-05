0b82cbadc5e20d99bef752d993d2ea5ba1b3a3b5
Brendan <brendan.dcruze@phenompeople.com>
2020-12-05 12:07:08 +0530

    Part 2: Solution

---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,38 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        if (restaurantName.isEmpty() || restaurants.isEmpty()) {
+            return null;
+        }
+        for (Restaurant restaurantObj : restaurants) {
+            if (restaurantObj.getName().toLowerCase().trim().equals(restaurantName.toLowerCase().trim())) {
+                return restaurantObj;
+            }
+        }
+        throw new restaurantNotFoundException(restaurantName);
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        if (restaurantToBeRemoved == null)
+            throw new restaurantNotFoundException(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
